89a4b9caf11b8e11fe56fa76db5c699b74e98879
vytdo <vytdo1999@gmail.com>
2023-02-21 10:27:42 +1100

    modified so hello world is on same line

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -11,7 +11,8 @@ using namespace std;
 int main(int argc, char* argv[]) {
     for (int i = 0; i < argc; i++)
     {
-        cout << argv[i] << endl;
+        cout << argv[i] << " ";
     }
 
+    return 0;
 }
